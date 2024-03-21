0c5c55add55e86a88923f4731de5ed25f969d247
zxqin <1075912003@qq.com>
2024-03-21 10:26:30 +0800

    完成了磁盘分区信息的创建与发送

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -5,6 +5,7 @@
 #include "framework.h"
 #include "RemoteCtrl.h"
 #include "ServerSocket.h"
+#include <direct.h>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -16,6 +17,34 @@
 CWinApp theApp;
 
 using namespace std;
+void Dump(BYTE* pData, size_t nSize)
+{
+    std::string strOut;
+    for (size_t i = 0; i < nSize; i++) {
+        char buf[8] = "";
+        if (i > 0 && (i % 16 == 0))
+            strOut += "\n";
+        snprintf(buf, sizeof(buf), "%02X ", pData[i] & 0xFF);
+        strOut += buf;
+    }
+    strOut += "\n";
+    OutputDebugStringA(strOut.c_str());
+}
+
+int MakeDriverInfo() {//创建磁盘分区信息,1代表A盘，2代表B盘...26代表Z盘
+    std::string result;
+    for (int i = 1; i <= 26; i++) {
+        if (_chdrive(i) == 0) {
+            if (result.size() > 0)
+                result += ',';
+            result += 'A' + i - 1;
+        }
+    }
+    CPacket pack(1, (BYTE*)result.c_str(), result.size());//打包用的
+    Dump((BYTE*)pack.Data(), pack.Size() + 6);
+    //CServerSocket::getInstance()->Send(pack);
+    return 0;
+}
 
 int main()
 {
@@ -34,33 +63,38 @@ int main()
         }
         else
         {
-            //1 进度的可控性 2 对接的方便性 3 可行性评估，提早暴漏风险
-            // TODO: socket,bind,listen,accept,read,write,close
-            //套接字初始化
+            ////1 进度的可控性 2 对接的方便性 3 可行性评估，提早暴漏风险
+            //// TODO: socket,bind,listen,accept,read,write,close
+            ////套接字初始化
+            //CServerSocket* pserver = CServerSocket::getInstance();//服务器创建单例
+            //int count = 0;
+            //if (pserver->InitSocker() == false) {//初始化
+            //    MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状态！"), _T("网络初始化失败！"), MB_OK | MB_ICONERROR);
+            //    exit(0);
+            //}
+            //while (CServerSocket::getInstance() != NULL) {
+            //    if (pserver->AcceptClient() == false) {
+            //        if (count >= 3) {
+            //            MessageBox(NULL, _T("多次无法接入用户，结束程序！"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
+            //            exit(0);
+            //        }
+            //        MessageBox(NULL, _T("无法正常接入用户，自动重试"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
+            //        count++;
+            //    }
+            //    int ret = pserver->DealCommand();
+            //    //TODO:
+            //}
+            ////静态变量
+            ////在第一次调用的时候初始化，在程序销毁的时候被销毁
+            ////全局静态变量
+            ////在main函数之前被初始化，在整个程序结束之后被析构
+            int nCmd = 1;
+            switch (nCmd) {
+            case 1://查看磁盘分区
+                MakeDriverInfo();
+                break;
 
-            CServerSocket* pserver = CServerSocket::getInstance();//服务器创建单例
-            int count = 0;
-            if (pserver->InitSocker() == false) {//初始化
-                MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状态！"), _T("网络初始化失败！"), MB_OK | MB_ICONERROR);
-                exit(0);
             }
-            while (CServerSocket::getInstance() != NULL) {
-                if (pserver->AcceptClient() == false) {
-                    if (count >= 3) {
-                        MessageBox(NULL, _T("多次无法接入用户，结束程序！"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
-                        exit(0);
-                    }
-                    MessageBox(NULL, _T("无法正常接入用户，自动重试"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
-                    count++;
-                }
-                int ret = pserver->DealCommand();
-                //TODO:
-            }
-            //静态变量
-            //在第一次调用的时候初始化，在程序销毁的时候被销毁
-            //全局静态变量
-            //在main函数之前被初始化，在整个程序结束之后被析构
-             
 
         }
     }
--- RemoteCtrl/RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/RemoteCtrl/ServerSocket.h
@@ -2,10 +2,26 @@
 #include "pch.h"
 #include "framework.h"
 
+#pragma pack(push)
+#pragma pack(1)//告诉编译器将每个成员变量的对齐设为1字节
+
 class CPacket
 {
 public:
 	CPacket():sHead(0),nLength(0),sCmd(0),sSum(0) {}
+	CPacket(WORD nCmd, const BYTE* pData, size_t nSize) {
+		sHead = 0xFEFF;
+		nLength = nSize + 4;
+		sCmd = nCmd;
+		strData.resize(nSize);
+		memcpy((void*)strData.c_str(), pData, nSize);
+		sSum = 0;
+		for (size_t j = 0; j < strData.size(); j++)
+		{
+			sSum += BYTE(strData[j]) & 0xFF;
+		}
+
+	}
 	CPacket(const CPacket& pack) {
 		sHead = pack.sHead;
 		nLength = pack.nLength;
@@ -42,7 +58,7 @@ public:
 		i += 2;
 		WORD sum = 0;
 		for (size_t j = 0; j < strData.size(); j++) {
-			sum += BYTE(strData[i]) & 0xFF;
+			sum += BYTE(strData[j]) & 0xFF;
 		}
 		if (sum == sSum) {
 			nSize = i;//head,length,data...
@@ -63,12 +79,27 @@ public:
 		else
 			return *this;
 	}
+	int Size() {//包数据的大小
+		return nLength + 6;
+	}
+	const char* Data() {
+		strOut.resize(nLength + 6);
+		BYTE* pData = (BYTE*)strOut.c_str();
+		*(WORD*)pData = sHead; pData += 2;
+		*(DWORD*)pData = nLength; pData += 4;
+		*(WORD*)pData = sCmd; pData += 2;
+		memcpy(pData, strData.c_str(), strData.size()); pData += strData.size();
+		*(WORD*)pData = sSum;
+		return strOut.c_str();
+	}
+
 public:
 	WORD sHead;//包头，固定位FE FF
 	DWORD nLength;//包长度（从控制命令开始，到和校验结束）
 	WORD sCmd;//控制命令
 	std::string strData;//包数据
 	WORD sSum;//和校验
+	std::string strOut;//整个包的数据
 };
 
 class CServerSocket
@@ -134,6 +165,10 @@ public:
 	const bool Send(char* pData, int nSize) {
 		if (m_client == -1)return false;
 		return (send(m_client, pData, nSize, 0)) > 0;
+	}
+	bool Send(CPacket& pack) {
+		if (m_client == -1)return false;
+		return (send(m_client, pack.Data(), pack.Size(), 0)) > 0;
 	}
 
 private:
